82ec7d3ff74a139c3d2681b826c81f780d9cbc6b
Rugved Koshiya <rugvedkoshiya2002@gmail.com>
2022-01-30 17:27:01 +0530

    enviroment setup

---
+++ models/config.py
@@ -0,0 +1,20 @@
+from os import getenv
+
+class Config():
+    DEBUG = getenv("DEBUG")
+    DOCUMENTATION_LINK = "https://gmtv.stoplight.io/docs/v1/overview.json"
+    SECRET_KEY = getenv("SECRET_KEY")
+    MONGO_LINK = getenv("MONGO_LINK")
+    MONGO_USERNAME = getenv("MONGO_USERNAME")
+    MONGO_PASSWORD = getenv("MONGO_PASSWORD")
+    DATABASE_NAME = getenv("DATABASE_NAME")
+    TV_COLLECTION_NAME = "TV"
+    MOVIE_COLLECTION_NAME = "Movie"
+    USER_COLLECTION_NAME = "Users"
+    USER_DATA_COLLECTION_NAME = "UserData"
+    IP_LOOKUP_WEBSITE = "http://ip-api.com/json/"
+    SUPER_USER = getenv("SUPER_USER")
+    ADMIN_USER = getenv("ADMIN_USER")
+    WORKER_USER = getenv("WORKER_USER")
+    VERSION = "v1"
+    PAGING = 20
