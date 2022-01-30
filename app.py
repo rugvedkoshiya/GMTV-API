735993b885a49749ee386b4e14e9ae8f016d3aee
Rugved Koshiya <rugvedkoshiya2002@gmail.com>
2022-01-30 17:24:22 +0530

    gitignore

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ import routes.route_movie
 import routes.route_tv
 import routes.route_user
 from models.config import Config as SETTING
+from dotenv import load_dotenv
 
 if __name__ == "__main__":
+    # print(os.environ)
+    load_dotenv()
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
     app.run(port=5000, debug=SETTING.DEBUG)
