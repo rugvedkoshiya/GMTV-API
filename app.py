c4aef80460ee4dfd23305192c9a5304d907cb08c
Rugved Koshiya <rugvedkoshiya2002@gmail.com>
2022-01-30 17:34:54 +0530

    enviroment setup

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+
 from swaggerConfig import app
 import os
 import routes.route_auth
@@ -5,10 +9,8 @@ import routes.route_movie
 import routes.route_tv
 import routes.route_user
 from models.config import Config as SETTING
-from dotenv import load_dotenv
+
 
 if __name__ == "__main__":
-    # print(os.environ)
-    load_dotenv()
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
     app.run(port=5000, debug=SETTING.DEBUG)
